96411985a94deefd052dbd3048e7e4329d59f5e7
akr9757 <anilkumareddy9757@gmail.com>
2024-08-31 15:14:10 +0530

    garafana dashboard configuration

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "key_id" {
+  default = "arn:aws:kms:us-east-1:975050250738:key/581c3619-7ba5-4a58-833d-0a657d809e15"
+}
+
+variable "parameters" {
+  default = {
+    "dev.expense.rds.username" = "admin1"
+    "dev.expense.rds.endpoint" = "dev-expense-rds.clcucwu2kydp.us-east-1.rds.amazonaws.com"
+    "dev.expense.frontend.backend_endpoint" = "https://backend-dev.akrdevops.online/"
+    "dev.expense.rds.password" = "ExpenseApp123"
+    "ssh.password" = "DevOps321"
+    "ssh.username" = "centos"
+    "ses.username" = "AKIA6GBMGDHZLSFQTQRT"
+    "ses.password" = "BLnHVQaMXacIJWNVxvDlDIpOsEV/VItJJJszChNEcCVs"
+  }
+}
+
+variable "secrets" {
+  default = {
+    "dev.expense.rds.not" = "ExpenseApp123"
+  }
+}
